9353d0c16e63192ddf857407ffcc96c9273dbe59
talya meizlish <you@example.com>
2023-11-10 01:31:38 +0200

    connect register to backend

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,25 +1,52 @@
 import "./register.css"
 import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-// import RoomSharpIcon from '@material-ui/icons/RoomSharp';
+//import Typography from "@mui/material/Typography";
 import RoomSharpIcon from '@mui/icons-material/RoomSharp';
+import { useRef, useState } from "react";
+import axios from "axios";
+
 
 export default function Register() {
+
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+  const nameRaf = useRef()
+  const emailRaf = useRef()
+  const passwordRaf = useRef()
+
+  const habdleSubmit= async (e) => {
+    e.preventDefault();
+    const newUser = {
+      username : nameRaf.current.value,
+      email : emailRaf.current.value,
+      password : passwordRaf.current.value,
+    };
+    try{
+      await axios.post("/users/register", newUser);
+      setError(false);
+      setSuccess(true)
+    }catch(err){
+      console.log(err)
+      console.log(newUser.username)
+      setError(true);
+    }
+  }
+
   return (
     <div className="registerContainer">
         <div className="logo">
           <Grid item xs={1}>
             <RoomSharpIcon/>
           </Grid>
-          <Grid item xs={1}>
-            <Typography>LamaPin</Typography>
-          </Grid>
+          LamaPin
         </div>
-        <form>
-            <input type="text" placeholder="username"/>
-            <input type="email" placeholder="email"/>
-            <input type="password" placeholder="password"/>
-            <button>Register</button>
+        <form onSubmit={habdleSubmit}>
+          <input type="text" placeholder="username" ref={nameRaf}/>
+          <input type="email" placeholder="email" ref={emailRaf}/>
+          <input type="password" placeholder="password" ref={passwordRaf}/>
+          <button className="registerBtn">Register</button>
+          {success && <span className="success">Successfull. You can login now</span>}
+          {error && <span className="failure">Something went wrong</span>}
         </form>
     </div>
   )
